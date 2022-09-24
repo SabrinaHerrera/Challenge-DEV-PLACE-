d3152ad117d0a5983faa06fa8a5b80fd2f8c09d6
Sabrina <sabrinaherrera199@gmail.com>
2022-09-24 10:28:26 -0300

    version3

--- Modulo Algoritmo/Ejercicio2/script.js
+++ Modulo Algoritmo/Ejercicio2/script.js
@@ -20,3 +20,16 @@ const btnVerificar = document.getElementById('btn-verificar')
    // document.getElementById('conf-pass').display='block';
 
 //}
+//La condicion
+if(password !== confPass1 && password !== confPass2 && password !== confPass3){
+  alert("Tenes que ejercitar la memoria");
+}else{
+  alert("Felicitaciones, recordas tu contraseña");
+}
+
+
+//Comentario: Lastimosamente Me quede sin tiempo y recursos,
+// mi intencion era que en la pantalla principal
+// aparezca el imput de contraseña y el boton enviar
+//al oprimir el boton el imput + el boton debian desaparecer
+//y aparecer solo los 3 imput de confirmacion + el boton verificar
