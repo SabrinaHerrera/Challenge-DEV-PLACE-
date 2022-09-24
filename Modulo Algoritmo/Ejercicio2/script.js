c5c62b443a8ebab43a3f27b573d1b031c48294c9
Sabrina <sabrinaherrera199@gmail.com>
2022-09-24 08:32:18 -0300

    first commit

---
+++ Modulo Algoritmo/Ejercicio2/script.js
@@ -0,0 +1,22 @@
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+
+const confPass1 = document.getElementById('conf-password1')
+const confPass2 = document.getElementById('conf-password2')
+const confPass3 = document.getElementById('conf-password3')
+
+const btnVerificar = document.getElementById('btn-verificar')
+
+
+
+//button.addEventListener('click'){
+//    const pass = password.value;
+//}
+
+
+//function enviar(){
+
+  //  document.getElementById('ingresar').display='none';
+   // document.getElementById('conf-pass').display='block';
+
+//}
